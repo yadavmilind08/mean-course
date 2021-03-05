59489cac98f42bf05e14aba45b961fa7ab9f45a2
Milind Yadav <milind.yadav@nitorinfotech.net>
2021-03-05 17:22:03 +0530

    Uploading Files

--- src/app/posts/posts.servicet.ts
+++ src/app/posts/posts.servicet.ts
@@ -43,12 +43,21 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title: title, content: content };
-    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
+    this.http.post<{ message: string, postId: string }>(
+      'http://localhost:3000/api/posts',
+      postData
+    )
       .subscribe((responseData) => {
-        const id = responseData.postId;
-        post.id = id;
+        const post: Post = {
+          id: responseData.postId,
+          title: title,
+          content: content
+        }
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(["/"]);
