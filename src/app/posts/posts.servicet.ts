8ab9d103d2aff2143b46edb38e90ca17824cf651
Milind Yadav <yadavmilind08@gmail.com>
2021-03-06 17:07:22 +0530

    Fetched images to frontend

--- src/app/posts/posts.servicet.ts
+++ src/app/posts/posts.servicet.ts
@@ -23,7 +23,8 @@ export class PostsService {
           return {
             id: post._id,
             title: post.title,
-            content: post.content
+            content: post.content,
+            imagePath: post.imagePath
           }
         });
       }))
@@ -48,15 +49,16 @@ export class PostsService {
     postData.append('title', title);
     postData.append('content', content);
     postData.append('image', image, title);
-    this.http.post<{ message: string, postId: string }>(
+    this.http.post<{ message: string, post: Post }>(
       'http://localhost:3000/api/posts',
       postData
     )
       .subscribe((responseData) => {
         const post: Post = {
-          id: responseData.postId,
+          id: responseData.post.id,
           title: title,
-          content: content
+          content: content,
+          imagePath: responseData.post.imagePath
         }
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
@@ -65,7 +67,7 @@ export class PostsService {
   }
 
   updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+    const post: Post = { id: id, title: title, content: content, imagePath: null };
     this.http.put('http://localhost:3000/api/posts/' + id, post)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
