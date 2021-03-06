2eacb2fe376169c8f40a6d11d476dc03afe2a044
Milind Yadav <yadavmilind08@gmail.com>
2021-03-06 21:55:44 +0530

    Added pagination on backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -50,7 +50,13 @@ router.post(
 );
 
 router.get("", (req, res, next) => {
-  Post.find().then((documents) => {
+  const pageSize = +req.query.pageSize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  postQuery.then((documents) => {
     res.status(200).json({
       message: "Posts fetched successfully!",
       posts: documents,
